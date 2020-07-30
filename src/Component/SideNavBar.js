096f1ceb288d9496a9d13b42694724ea794df0ba
prageeth2020 <prageethpramuditha.2020@gmail.com>
2020-07-31 00:20:08 +0530

    edited

--- src/Component/SideNavBar.js
+++ src/Component/SideNavBar.js
@@ -35,36 +35,8 @@ class SideNavPage extends Component {
                         <nav className="navbar navbar-expand-lg navbar-light bg-primary border-bottom text-white">
                             <button className="btn btn-secondary" id="menu-toggle" onClick={this.viewSideBar}><i className="fas fa-align-justify"></i></button>
 
-                            <button className="navbar-toggler" type="button" data-toggle="collapse"
-                                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                                    aria-expanded="false" aria-label="Toggle navigation">
-                                <span className="navbar-toggler-icon"></span>
-                            </button>
                             <p style={{margin : "auto"}} className="mx-2"> Dashboard ></p>
-                            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                                <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
-                                    <li className="nav-item active">
-                                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link" href="#">Link</a>
-                                    </li>
-                                    <li className="nav-item dropdown">
-                                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown"
-                                           role="button" data-toggle="dropdown" aria-haspopup="true"
-                                           aria-expanded="false">
-                                            Dropdown
-                                        </a>
-                                        <div className="dropdown-menu dropdown-menu-right"
-                                             aria-labelledby="navbarDropdown">
-                                            <a className="dropdown-item" href="#">Action</a>
-                                            <a className="dropdown-item" href="#">Another action</a>
-                                            <div className="dropdown-divider"></div>
-                                            <a className="dropdown-item" href="#">Something else here</a>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
+
                         </nav>
                         <Dashboard/>
 
